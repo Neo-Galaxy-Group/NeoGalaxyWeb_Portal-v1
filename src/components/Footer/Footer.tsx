4b228849dcfee7fb5886e7c95ab53a7f293d6fa4
LittleShyStar02 <65179413+LittleShyStar02@users.noreply.github.com>
2026-03-24 09:59:19 +0100

    update pages

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,15 +6,21 @@ function Footer() {
 
   return (
     <footer>
-      <div className="Footer-Top">
+      <div className="Footer-Bottom">
+        © Copyright {creationYear}{" "}
+        {creationYear !== currentYear && ` - ${currentYear}`}
+      </div>
+    </footer>
+  );
+}
+
+/*
+<div className="Footer-Top">
         <div className="Footer-TopLeft">
         </div>
         <div className="Footer-TopRight">
         </div>
       </div>
-      <div className="Footer-Bottom">© Copyright {creationYear} {creationYear !== currentYear && ` - ${currentYear}`}</div>
-    </footer>
-  );
-}
+*/
 
 export default Footer;

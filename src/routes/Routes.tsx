9c1335d721bbd7c5f24a310d89cd6716770ec88e
LittleShyStar02 <65179413+LittleShyStar02@users.noreply.github.com>
2026-03-27 11:06:16 +0100

    logo update

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -7,10 +7,10 @@ import Community from "../pages/Community/Community"
 import Home from "../pages/Home/Home"
 import SoftwareHouse from "../pages/SoftwareHouse/SoftwareHouse"
 
-import AetheriaMCImg from "../assets/img/neogalaxylogo.png"
-import CommunityImg from "../assets/img/neogalaxylogo.png"
-import HomeImg from "../assets/img/neogalaxylogo.png"
-import SoftwareHouseImg from "../assets/img/neogalaxylogo.png"
+import AetheriaMCImg from "../assets/img/aetheriamc_logo.png"
+import CommunityImg from "../assets/img/community_logo.png"
+import HomeImg from "../assets/img/ecosystem_logo.png"
+import SoftwareHouseImg from "../assets/img/softwarehouse_logo.png"
 
 
 const router = createBrowserRouter([
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/projects/softwarehouse",
-    element: <PageTemplate children={<SoftwareHouse />} imgSrc={SoftwareHouseImg} text="Da decidere" />
+    element: <PageTemplate children={<SoftwareHouse />} imgSrc={SoftwareHouseImg} text="La nostra Software House" />
   },
 ]);
 
